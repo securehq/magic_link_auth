bf09ed25bb21693c06897eb06856588457a2903b
Melvin Sembrano <melv@hey.com>
2026-05-01 10:49:50 +1000

    remove redundant initializer

--- lib/magic_link_auth/engine.rb
+++ lib/magic_link_auth/engine.rb
@@ -5,13 +5,5 @@ module MagicLinkAuth
     config.generators do |g|
       g.test_framework nil
     end
-
-    initializer "magic_link_auth.action_mailer" do
-      ActiveSupport.on_load(:action_mailer) do
-        MagicLinkAuth::ApplicationMailer.default(
-          from: -> { MagicLinkAuth.configuration.resolved_mailer_from }
-        )
-      end
-    end
   end
 end
